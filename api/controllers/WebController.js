9f6113e7993d5c559705467d1dde99405d746c60
iansuny <iansuny@yahoo.com.tw>
2016-08-13 02:15:39 +0800

    add many pages

--- api/controllers/WebController.js
+++ api/controllers/WebController.js
@@ -18,9 +18,29 @@ module.exports = {
 				return res.view('blank', {
 					layout: 'index',
 				})
+			case 'humix-think':
+				return res.view('blank', {
+					layout: 'humix-think',
+				})
+			case 'humix-sense':
+				return res.view('blank', {
+					layout: 'humix-sense',
+				})
+			case 'example':
+				return res.view('blank', {
+					layout: 'example',
+				})
 			case 'feature':
 				return res.view('blank', {
 					layout: 'feature',
+				})
+			case 'documentation':
+				return res.view('blank', {
+					layout: 'documentation',
+				})
+			case 'support':
+				return res.view('blank', {
+					layout: 'support',
 				})
 		}
 	}
